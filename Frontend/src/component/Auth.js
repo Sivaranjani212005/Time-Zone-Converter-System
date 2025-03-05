4338d5a99b5cdb89270f2d12cad3d20f8dd9b0a8
Sivaranjani212005 <sivaranjanisshunmugaraj@gmail.com>
2025-03-05 10:57:57 +0530

    Auth.js

--- Frontend/src/component/Auth.js
+++ Frontend/src/component/Auth.js
@@ -1,26 +1,17 @@
 import React, { createContext, useContext, useState } from 'react';
-
 const AuthContext = createContext();
-
 export const useAuth = () => {
   return useContext(AuthContext);
 };
-
 export const AuthProvider = ({ children }) => {
   const [authData, setAuthData] = useState(null);
-
   const Login = (username, password) => {
-  
     console.log('Logging in with', username, password);
-    
     setAuthData({ username });
   };
-
-  const Logout = () => {
-    
+  const Logout = () => { 
     setAuthData(null);
   };
-
   return (
     <AuthContext.Provider value={{ authData, Login, Logout }}>
       {children}
