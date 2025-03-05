2dcd56e49492f370676d6c6919f5028faefeffd8
Sivaranjani212005 <sivaranjanisshunmugaraj@gmail.com>
2025-03-05 10:25:13 +0530

    app.js

--- Backend/app.js
+++ Backend/app.js
@@ -8,9 +8,6 @@ var usersRouter = require('./routes/users');
 const contactRouter = require('./routes/contactRoutes');
 const timeRouter = require('./routes/timeRoutes');
 const app = express();
-
-
-
 require('dotenv').config();
 mongoose.connect('mongodb://localhost:27017/timezone_converter')
 .then(() => {console.log('MongoDB connected')
@@ -26,11 +23,11 @@ app.use('/timezone_converter', usersRouter);
 app.use('/timezone_converter', contactRouter);
 app.use('/timezone_converter', timeRouter);
 app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
+   
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
   
-    // render the error page
+ 
     res.status(err.status || 500);
     res.render('error');
   });
